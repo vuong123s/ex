2348a523eaa92fe94eb135f078677045c5b09a11
vuong123s <khanhvuong123s@gmail.com>
2020-07-07 09:22:53 +0700

    Third Commit

--- src/App.js
+++ src/App.js
@@ -79,23 +79,19 @@ const App = (props) => {
             <Card style={{ width: '18rem' }}>
               <Card.Img variant="top" src={CardImg} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>May Tinh</Card.Title>
               </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
               <Card.Img variant="top" src={CardImg} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>May Tinh</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={CardImg} />
+              <Card.Body>
+                <Card.Title>May Tinh</Card.Title>
               </Card.Body>
             </Card>
             
